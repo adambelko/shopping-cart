bb9c297f94df801e30ca428d1dbcb14aca089427
Adzi <belkoadam@gmail.com>
2023-04-16 09:13:01 +1000

    saving work before hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,26 +9,44 @@ const Header = () => {
                     <span className="title-left">BOOK</span>
                     <span className="title-right">ADDICTS</span>
                 </h1>
-                <div className="header__cart">
-                    <div className="cart-img">
+                <div className="header__right">
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink to="/">Home</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/Store">Store</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/About">About</NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    <label className="header__hamburger-menu">
+                        <input type="checkbox" />
+                    </label>
+                    <aside className="sidebar">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <NavLink to="/">Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/Store">Store</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/About">About</NavLink>
+                                </li>
+                            </ul>
+                        </nav>
+                    </aside>
+                    <div className="header__cart-wrapper">
                         <img src={cart} alt="cart-img" />
                     </div>
-                    <div className="cart-badge">1</div>
+                    <div className="header__cart-badge">1</div>
                 </div>
             </div>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/Store">Store</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/About">About</NavLink>
-                    </li>
-                </ul>
-            </nav>
         </header>
     );
 };
