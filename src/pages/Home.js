a3e959b4cb6146557ccf7da966bce8efad3541ef
Adzi <belkoadam@gmail.com>
2023-04-18 07:34:18 +1000

    Main img with text overlay  in Home page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
+import sofa from "../assets/images/sofa.webp";
+
 const Home = () => {
     return (
         <div>
-            <h2>HOME CONTENT </h2>
+            <div className="home__img-wrapper">
+                <img src={sofa} alt="sofa-img" className="sofa-img" />
+                <div className="text-overlay">
+                    <h2>
+                        <span>
+                            A SOFA,
+                            <br />A GOOD
+                            <br /> BOOK,
+                            <br />
+                            AND YOU.
+                        </span>
+                    </h2>
+                </div>
+            </div>
         </div>
     );
 };
