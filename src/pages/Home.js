3394a13bff9d81d07127bd7a425cf401cbf58171
Adzi <belkoadam@gmail.com>
2023-04-19 09:04:12 +1000

    Gallery section layout and styles looking decent, mobile version not done yet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,13 +19,31 @@ const Home = () => {
                     </h2>
                 </div>
             </div>
-            <div className="home__bestsellers-title-wrapper">
+            <div className="home__gallery-title">
                 <span>BOOK ADDICTS</span>
                 <h2>BESTSELLERS</h2>
             </div>
             <div className="home__gallery">
-                <div className="gallery-bestsellers">
+                <div className="gallery-bestsellers-wrapper">
+                    <div className="gallery-bestsellers">
+                        <GallerySlider gallery={gallery} />
+                    </div>
+                </div>
+                <div className="home__gallery-title recommended-books">
+                    <span>This Month's</span>
+                    <h2>RECOMMENDED BOOKS</h2>
+                </div>
+                <div className="gallery-recommended">
                     <GallerySlider gallery={gallery} />
+                </div>
+                <div className="home__gallery-title gallery-last-part">
+                    <h2>
+                        THERE'S NO <br /> SUCH THING AS TOO <br />
+                        MANY BOOKS
+                    </h2>
+                    <button>
+                        <a href="/About">Read Our Story</a>
+                    </button>
                 </div>
             </div>
         </div>
