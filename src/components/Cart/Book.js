6740eeaad9f78a3f60dfd6d4aeaa578d4657e8f4
Adzi <belkoadam@gmail.com>
2023-04-30 11:30:09 +1000

    Cart layout and styles updated

--- src/components/Cart/Book.js
+++ src/components/Cart/Book.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { HiOutlineMinusSm } from "react-icons/hi";
+import { HiOutlinePlusSm } from "react-icons/hi";
 import { RxCross2 } from "react-icons/rx";
 
 import "../../styles/Cart/Book.scss";
@@ -14,16 +16,18 @@ const Book = (props) => {
                 />
                 <div className="book__left-wrapper">
                     <span className="book__title">{props.bookTitle}</span>
-                    <span className="book__price">{props.bookPrice}</span>
+                    <span className="book__price">${props.bookPrice}</span>
                     <div className="book__quantity-wrapper">
-                        <input type="text" />
+                        <HiOutlineMinusSm />
+                        <input type="text" defaultValue="1" />
+                        <HiOutlinePlusSm />
                     </div>
                 </div>
             </Link>
             <div className="book__right-wrapper">
                 <div className="book__total-price">$50</div>
                 <div className="book__remove-icon">
-                    <RxCross2 size={"26px"} />
+                    <RxCross2 size={"22px"} />
                 </div>
             </div>
         </div>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,13 @@
-import chairs from "../../assets/images/Books/99-Chairs.webp";
+import { useState } from "react";
+import { CiShoppingTag } from "react-icons/ci";
 import Book from "./Book";
+
+import chairs from "../../assets/images/Books/99-Chairs.webp";
 
 import "../../styles/Cart/Cart.scss";
 
 const Cart = () => {
+    const [promoIsOpen, setPromoIsOpen] = useState(false);
     return (
         <div className="cart">
             <div className="cart__list">
@@ -24,10 +28,52 @@ const Cart = () => {
                         />
                     </li>
                 </ul>
+                <hr />
+                <div className="cart__promo-code">
+                    <button
+                        className="cart__promo-code-btn"
+                        onClick={() => setPromoIsOpen(!promoIsOpen)}
+                    >
+                        <CiShoppingTag />
+                        <span>Enter a promo code</span>
+                    </button>
+                    {promoIsOpen && (
+                        <div className="cart__promo-code-wrapper">
+                            <input
+                                type="text"
+                                placeholder="Enter a promo code"
+                                className="cart__promo-code-input"
+                            />
+                            <button className="cart__promo-code-apply-btn">
+                                Apply
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
             <div className="cart__order-summary">
                 <div className="cart__section-title">Order Summary</div>
                 <hr />
+                <div className="cart__summary-subtotal">
+                    <dl>
+                        <dt>Subtotal</dt>
+                        <dd>$44</dd>
+                    </dl>
+                    <div className="cart__summary-shipping">
+                        <span>Flat Rate Shipping</span>
+                        <span>$8.50</span>
+                    </div>
+                </div>
+                <hr />
+                <div className="cart__summary-total">
+                    <dl>
+                        <dt>Total</dt>
+                        <dd>$65</dd>
+                    </dl>
+                    <button className="cart__checkout-btn" type="button">
+                        Checkout
+                    </button>
+                </div>
             </div>
         </div>
     );
